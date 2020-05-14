e8c61a67337f40ee4e08c12ace6df8f804f1e9c8
Milo Vahos <milovahos@gmail.com>
2020-05-14 17:22:14 -0500

    Post and PostList views added

---
+++ client/src/PostList.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export default () => {
+
+  const [posts, setPosts] = useState({});
+
+  const fetchPosts = async () => {
+    const res = await axios.get('http://localhost:4000/posts');
+    setPosts(res.data);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const renderPosts = Object.values(posts).map( post => {
+    return (
+      <div key={post.id}
+        className="card"
+        style={{ width: '30%', marginBottom: '20px' }}>
+          <div className="card-body">
+            <h3>{ post.title }</h3>
+          </div>
+      </div>
+    );
+  });
+
+  console.log(posts);
+
+  return <div className="d-flex flex-row flex-wrap justify-content-between">
+    { renderPosts }
+  </div>;
+};
