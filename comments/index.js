3b815a4a4208e11449248ba6be380a621d0ed63f
Milo Vahos <milovahos@gmail.com>
2020-05-19 12:57:38 -0500

    Avances en el curso

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyparser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app =  express();
 app.use(bodyparser.json());
@@ -13,14 +14,26 @@ app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
   const commentId = randomBytes(4).toString('hex');
   const { content } = req.body;
-
   const comments = commentsByPostId[req.params.id] || [];
   comments.push({ id: commentId, content });
   commentsByPostId[req.params.id] = comments;
+  await axios.post('http://localhost:4005/events', {
+    type: 'Comment Created',
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+    }
+  })
   res.status(201).send(comments);
+});
+
+app.post('/events', (req, res) => {
+  console.log('Event Received: ', req.body.type);
+  res.send({});
 });
 
 app.listen(4001, () => {
--- posts/index.js
+++ posts/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyparser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app =  express();
 app.use(bodyparser.json());
@@ -13,13 +14,22 @@ app.get('/posts', (req, res) => {
   res.send(posts);
 });
 
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
   const id = randomBytes(4).toString('hex');
   const { title } = req.body;
   posts[id] = {
     id, title
   };
+  await axios.post('http://localhost:4005/events', {
+    type: 'PostCreated',
+    data: { id, title },
+  });
   res.status(201).send(posts[id]);
+});
+
+app.post('/events', (req, res) => {
+  console.log('ReceivedEvent', req.body.type);
+  res.send({});
 });
 
 app.listen(4000, () => {
