65a02b5e1012f1a73a991568f94605f463c4bb8c
Milo Vahos <milovahos@gmail.com>
2020-05-18 20:19:20 -0500

    Simple event bus implementation

---
+++ event-bus/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyparser.json());
+
+app.post('/events', (req, res) => {
+  const event = req.body;
+  axios.post('http://localhost:4000/events', event);
+  axios.post('http://localhost:4001/events', event);
+  axios.post('http://localhost:4002/events', event);
+  res.send({ status: 'OK' });
+});
+
+app.listen(4005, () => {
+  console.log('Listen on 4005');
+});
