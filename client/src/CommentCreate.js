5ae3f6209d0818fc676a43ab228ad16fbe339d63
Milo Vahos <milovahos@gmail.com>
2020-05-18 20:12:35 -0500

    Finish web app

---
+++ client/src/CommentCreate.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+export default ({ postId }) => {
+
+  const [content, setContent] = useState('');
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await axios.post(`http://localhost:4001/posts/${postId}/comments`,{
+      content
+    });
+    setContent('');
+  };
+
+  return <div>
+    <form onSubmit={onSubmit}>
+      <div className="form-group">
+        <label>New comment</label>
+        <input className="form-control" onChange={ e => setContent(e.target.value) }></input>
+      </div>
+      <button className="btn btn-primary">Submit</button>
+    </form>
+  </div>
+};
--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import CommentCreate from './CommentCreate';
+import CommentList from './CommentList';
 
 export default () => {
 
@@ -21,6 +23,8 @@ export default () => {
         style={{ width: '30%', marginBottom: '20px' }}>
           <div className="card-body">
             <h3>{ post.title }</h3>
+            <CommentList postId={post.id} />
+            <CommentCreate postId={post.id} />
           </div>
       </div>
     );
